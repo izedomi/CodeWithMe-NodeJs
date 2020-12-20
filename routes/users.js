31bc87516188f59bb43eaa0dbf4300530c8ee86c
Emmanuel Izedomi <kingemmanuel4life@gmail.com>
2020-12-20 20:30:50 +0100

    Added login/register routes and views

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/login', function(req, res, next){
+    res.render('login', {title: "Login to account"})
+});
+
+router.get('/register', function(req, res, next){
+  res.render('register', {title: "Register"})
+});
+
+
+
 module.exports = router;
