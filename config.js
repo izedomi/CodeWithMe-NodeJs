031e701ade1d179d5f446888beadd3bf1644b1ab
Izedomi Emmanuel <kingemmanuel4life@gmail.com>
2021-01-05 02:53:09 +0100

    change config variable to prod

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    'env': 'dev',
+    'env': 'prod',
     'sessionKey': 'HelloCodeWithMe',
     url: {
         'prod': 'https://code-with-me-app.herokuapp.com',
